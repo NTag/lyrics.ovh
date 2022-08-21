5fe3b82fb43bdc10dd8f55013d85c50206393635
Basile Bruneau <basile@bruneau.email>
2022-08-21 19:19:15 +0100

    fix: stop logging all errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const alltomp3 = require('alltomp3');
-const request = require('request-promise');
-const cors = require('cors')
+const express = require("express");
+const alltomp3 = require("alltomp3");
+const request = require("request-promise");
+const cors = require("cors");
 var things = [];
 let appApi = express();
 let appFrontend = express();
@@ -10,33 +10,36 @@ const portFrontend = 8081;
 
 appApi.use(cors());
 
-appApi.get('/v1/:artist/:title', function (req, res) {
+appApi.get("/v1/:artist/:title", function (req, res) {
   if (!req.params.artist || !req.params.title) {
-    return res.status(400).send({error: "Artist or title missing"});
+    return res.status(400).send({ error: "Artist or title missing" });
   }
-  alltomp3.findLyrics(req.params.title, req.params.artist).then(l => {
-    res.send({lyrics: l});
-  }).catch(e => {
-    console.log(e);
-    res.status(404).send({error: "No lyrics found"});
-  });
+  alltomp3
+    .findLyrics(req.params.title, req.params.artist)
+    .then((l) => {
+      res.send({ lyrics: l });
+    })
+    .catch((e) => {
+      // console.log(e);
+      res.status(404).send({ error: "No lyrics found" });
+    });
 });
 
-appApi.get('/suggest/:term', function (req, res) {
+appApi.get("/suggest/:term", function (req, res) {
   request({
-    uri: 'http://api.deezer.com/search?limit=15&q=' + req.params.term,
-    json: true
-  }).then(results => {
+    uri: "http://api.deezer.com/search?limit=15&q=" + req.params.term,
+    json: true,
+  }).then((results) => {
     res.send(results);
   });
 });
 
-appFrontend.use(express.static('frontend'));
+appFrontend.use(express.static("frontend"));
 
 appApi.listen(portApi, function () {
-  console.log('API listening on port ' + portApi);
+  console.log("API listening on port " + portApi);
 });
 
 appFrontend.listen(portFrontend, function () {
-  console.log('Frontend listening on port ' + portFrontend);
+  console.log("Frontend listening on port " + portFrontend);
 });
